a709f25cca0d12adad93af90de55417b4ecf6dfb
Alejandro9kgm <alejandro9kgm@gmail.com>
2024-06-09 13:28:45 +0200

    Add new reaction
    Saurita's first reaction has been added to a message

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const { loadCommands, loadEvents } = require("./helpers");
 require("dotenv").config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 client.commands = new Collection();
 
 loadCommands(client);
 loadEvents(client);
 
 client.login(process.env.DISCORD_TOKEN);
+
